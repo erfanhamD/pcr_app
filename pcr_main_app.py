9db11132a7d65a169d0c720a9196a0f0878206b4
erfanhamD <erfan.hamdi@gmail.com>
2022-04-05 10:02:29 +0430

    initial commit

---
+++ pcr_main_app.py
@@ -0,0 +1,21 @@
+from PyQt5 import QtWidgets, uic
+import sys
+
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi('mainwindow.ui', self)
+        self.btn_save.clicked.connect(self.btn_save_func)
+        self.btn_cancel.clicked.connect(app.instance().quit)
+        self.show()
+    def btn_save_func(self):
+        self.lbl_test.setText(self.le_name.text())
+
+
+
+        
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = Ui()
+    app.exec_()
