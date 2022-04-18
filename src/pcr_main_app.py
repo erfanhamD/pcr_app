6d5c77b22bb8a098813a92d310b8396fad7444ee
erfanhamD <erfan.hamdi@gmail.com>
2022-04-18 13:00:46 +0430

    added the method to read sensor data and plot it live

--- src/pcr_main_app.py
+++ src/pcr_main_app.py
@@ -2,6 +2,11 @@ from PyQt5 import QtWidgets, uic, QtCore
 import sys
 from pyqtgraph import PlotWidget
 import numpy as np
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+address = 0x08
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):    
@@ -17,12 +22,26 @@ class Ui(QtWidgets.QMainWindow):
         self.lbl_test.setText(self.le_name.text())
 
     def plot(self, hour, temperature):
-        self.graph_cycle.plot(hour, temperature)
+        self.plot_ref = self.graph_cycle.plot(hour, temperature)
         self.graph_cycle.setLabel('left', 'Temperature', units='C')
         self.graph_cycle.setLabel('bottom', 'Cycle Number')
         self.graph_cycle.setTitle('Cycling Graph')
         self.graph_cycle.showGrid(x=True, y=True)
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(200)
+        self.timer.timeout.connect(self.update_plot)
+        self.timer.start()
 
+    def read_data(self):
+        res = ""
+        res += bus.read_i2c_block_data(address, 0x00, 4)
+        return res.encode('utf-8')
+    
+    def update_plot(self):
+        self.x = []
+        self.y.append(self.read_data())
+        self.plot_ref.setData(self.x, self.y)
+        
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = Ui()
