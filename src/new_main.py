96346ebd3dfdd1ef92564052093c7754eba7757b
erfanhamdi <erfan.hamdi@gmail.com>
2022-11-25 11:04:47 +0330

    ac curves added, the plotting shows all of the plots

--- src/new_main.py
+++ src/new_main.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets, uic, QtCore, QtGui
 from PyQt5.QtCore import QObject, QThread
 import sys
-
+from contextlib import contextmanager
 from datetime import datetime
 from pyqtgraph import PlotWidget
 import pyqtgraph
@@ -105,15 +105,18 @@ class Worker(QObject):
         print(self.iter)
         # Calling the realtime image processing function
         print("Going to process the image")
+        global intensity_list
         intensity_list = image_processing(img_address)
-        self.intensity_master_list.append(intensity_list)
+        # print(f"this is the intensity list {intensity_list}")
+        # self.intensity_master_list = intensity_list
+        f_intensity[str(self.iter)] = intensity_list
 #         img = cv2.imread(f"{img_address}.jpeg", cv2.IMREAD_GRAYSCALE)
 #         cv2.imwrite(img_address_out, img)
         print("Image Captured and Processed")
         self.finished.emit()
 
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-
+f_intensity = pd.DataFrame([0]*45)
 class Ui(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):        
         super(Ui, self).__init__(*args, **kwargs)
@@ -123,7 +126,7 @@ class Ui(QtWidgets.QMainWindow):
         self.lineEdit.setText("Test")
         self.btn_save.clicked.connect(self.btn_save_func)
         self.btn_cancel.clicked.connect(app.instance().quit)
-        self.intensity_master_list = []
+        self.intensity_master_list = [0]*45
         # Plot Style
         self.graph_cycle.setLabel('left', 'Temperature', units='C')
         self.graph_cycle.setLabel('bottom', 'Time')
@@ -131,13 +134,24 @@ class Ui(QtWidgets.QMainWindow):
         #self.graph_cycle.setYRange(55, 100, padding=0)
         self.graph_cycle.setBackground('w')
         self.graph_cycle.showGrid(x=True, y=True)
+        # graph_ac plotting graph
+        self.graph_ac.setLabel('left', 'Intensity', units='a.u.')
+        self.graph_ac.setLabel('bottom', 'Cycle')
+        self.graph_ac.setTitle('Amplification Curve')
+        self.graph_ac.setBackground('w')
+        self.graph_ac.showGrid(x=True, y=True)
 
         # Ploting the Temprature for 1000 steps of time
         self.temp = [0]*1000
         self.time = [0]*1000
+        self.cycle = np.zeros
+        self.f_intensity = pd.DataFrame()
         self.dt = 1000 # ms
         pen = pyqtgraph.mkPen(color=(255, 0, 0))
         self.plot_ref = self.graph_cycle.plot(self.temp, self.time, pen=pen)
+        self.ac_plot_ref = []
+        for i in range(45):
+            self.ac_plot_ref.append(self.graph_ac.plot(pen = pen))
         self.tab_main.setCurrentIndex(0)
         self.btn_start.clicked.connect(self.send_command)
         self.show()
@@ -213,12 +227,19 @@ class Ui(QtWidgets.QMainWindow):
             self.worker.finished.connect(self.worker.deleteLater)
             self.thread.finished.connect(self.thread.deleteLater)
             self.thread.start()
-            intensity_file = open(f'{self.lineEdit.text()}/intensity.csv', 'w', newline = '')
+            # self.f_intensity[str(cycle_number)] = self.intensity_master_list
+#             self.f_intensity = np.c_[self.f_intensity, self.intensity_master_list]
+            try:
+                print(f"f_intensity shape is {f_intensity.shape}")
+                print(f_intensity.head())
+            except:
+                print("problem in df")
+#             intensity_file = open(f'{self.lineEdit.text()}/intensity.csv', 'w', newline = '')
 #             self.intensity_master_list = self.intensity_master_list[-1]
 #             print(f"*** {self.intensity_master_list[-1]}")
-            with intensity_file:
-                write = csv.writer(intensity_file)
-                write.writerows(self.intensity_master_list)
+#             with intensity_file:
+#                 write = csv.writer(intensity_file)
+#                 write.writerows(self.intensity_master_list)
 #             print(f"### {self.intensity_master_list}")
 #         print(self.intensity_master_list)
         #try:
@@ -232,9 +253,10 @@ class Ui(QtWidgets.QMainWindow):
         return float(current_temp)
 
     def update_plot_data(self):
-        self.time = self.time[1:]  # Remove the first y element.
+        # self.time = self.time[1:]  # Remove the first y element.
         self.time.append(self.time[-1] +1)  # Add a new value 1 higher than the last.
-        self.temp = self.temp[1:]  # Remove the first
+        # self.temp = self.temp[1:]  # Remove the first
+#         self.f_intensity = self.f_intensity[1:]
         try:
             in_read = ser.readline().decode('utf-8').rstrip()
             # A check to only accept full messages
@@ -252,6 +274,13 @@ class Ui(QtWidgets.QMainWindow):
             logging.info(e)
             sys.exit(1)
         self.plot_ref.setData(self.time, self.temp)  # Update the data.
+        # plot each row of the f_intensity dataframe
+        for idx, plot_ref in enumerate(self.ac_plot_ref):
+            plot_ref.setData(f_intensity.iloc[idx])
+        #self.plot_ref2.setData(self.time, self.f_intensity.iloc[:,1])
+        #self.plot_ref2.setData(self.time, self.f_intensity.iloc[:,2])
+
+#         self.ac_plot_ref.setData(self.cycle, self.f_intensity[:, -1])
 
 if __name__ == "__main__":
     # To save the log files, setting the name as the exact date time
