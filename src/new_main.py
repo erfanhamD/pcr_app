95e60671ccb51ef5a80e7e7b1a46576405fa408d
erfanhamdi <erfan.hamdi@gmail.com>
2022-11-28 10:53:27 +0330

    the problem with jumps were with the qt plot update dt which used to fall behind arduino

--- src/new_main.py
+++ src/new_main.py
@@ -106,7 +106,7 @@ class Worker(QObject):
         # Calling the realtime image processing function
         print("Going to process the image")
         global intensity_list
-        intensity_list = image_processing(img_address)
+        # intensity_list = image_processing(img_address)
         # print(f"this is the intensity list {intensity_list}")
         # self.intensity_master_list = intensity_list
         f_intensity[str(self.iter)] = intensity_list
@@ -115,11 +115,12 @@ class Worker(QObject):
         print("Image Captured and Processed")
         self.finished.emit()
 
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-f_intensity = pd.DataFrame([0]*45)
+
+
 class Ui(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):        
         super(Ui, self).__init__(*args, **kwargs)
+        print("Initializing the UI")
         # Loading the UI from the .ui file
         uic.loadUi('../ui_files/mainwindow.ui', self)
         # Reading the Assay name set by the user.
@@ -147,7 +148,7 @@ class Ui(QtWidgets.QMainWindow):
         self.time = [0]*1000
         self.cycle = np.zeros
         self.f_intensity = pd.DataFrame()
-        self.dt = 1000 # ms
+        self.dt = 100 # ms
         pen = pyqtgraph.mkPen(color=(255, 0, 0))
         # list of pens with 45 colors
         self.pens = [pyqtgraph.mkPen(color=(i, 45*1.3), width = 1) for i in range(45)]
@@ -175,10 +176,11 @@ class Ui(QtWidgets.QMainWindow):
 
     def send_command(self):
         # Reseting the buffer to read from the end of the buffer
-        ser.reset_input_buffer()
-        pred_time, pred_temp, den_time, den_temp, ext_temp, ext_time, ann_temp, ann_time = self.aggregate_params()
+        # ser.reset_input_buffer()
+        # pred_time, pred_temp, den_time, den_temp, ext_temp, ext_time, ann_temp, ann_time = self.aggregate_params()
         # params_str = f"{pred_time},{pred_temp},{den_time},{den_temp},{ext_time},{ext_temp},{ann_time},{ann_temp}>"
-        params_str = f"{pred_time},{pred_temp},{den_time}"
+        # params_str = f"{pred_time},{pred_temp},{den_time}"
+        params_str = "\n"
         ser.write(bytes(str(params_str), 'utf-8'))
         # Starting the timer used for realtime plotting of the temprature
         self.timer = QtCore.QTimer()
@@ -186,13 +188,13 @@ class Ui(QtWidgets.QMainWindow):
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
         # Printing the read params from arduino
-        print(ser.readline() )
+        # print(ser.readline() )
 
-    def read_serial(self):
-        while True:
-            ser.flushInput()
-            read = ser.readline().decode('utf-8').rstrip()
-            print(read)
+    # def read_serial(self):
+    #     while True:
+    #         ser.flushInput()
+    #         read = ser.readline().decode('utf-8').rstrip()
+    #         print(read)
     
     def btn_save_func(self):
         dir_path = os.path.join("/home/pi/pcr_app/src", f"{self.lineEdit.text()}")
@@ -224,13 +226,13 @@ class Ui(QtWidgets.QMainWindow):
             print(args)
             self.thread = QThread(parent = self)
             print(f"iter = {cycle_number}")
-            self.worker = Worker(cycle_number, self.lineEdit.text(), self.intensity_master_list)
-            self.worker.moveToThread(self.thread)
-            self.thread.started.connect(self.worker.run)
-            self.worker.finished.connect(self.thread.quit)
-            self.worker.finished.connect(self.worker.deleteLater)
-            self.thread.finished.connect(self.thread.deleteLater)
-            self.thread.start()
+            # self.worker = Worker(cycle_number, self.lineEdit.text(), self.intensity_master_list)
+            # self.worker.moveToThread(self.thread)
+            # self.thread.started.connect(self.worker.run)
+            # self.worker.finished.connect(self.thread.quit)
+            # self.worker.finished.connect(self.worker.deleteLater)
+            # self.thread.finished.connect(self.thread.deleteLater)
+            # self.thread.start()
             # self.f_intensity[str(cycle_number)] = self.intensity_master_list
 #             self.f_intensity = np.c_[self.f_intensity, self.intensity_master_list]
             try:
@@ -238,39 +240,28 @@ class Ui(QtWidgets.QMainWindow):
                 print(f_intensity.head())
             except:
                 print("problem in df")
-#             intensity_file = open(f'{self.lineEdit.text()}/intensity.csv', 'w', newline = '')
-#             self.intensity_master_list = self.intensity_master_list[-1]
-#             print(f"*** {self.intensity_master_list[-1]}")
-#             with intensity_file:
-#                 write = csv.writer(intensity_file)
-#                 write.writerows(self.intensity_master_list)
-#             print(f"### {self.intensity_master_list}")
-#         print(self.intensity_master_list)
-        #try:
-        #    print(float(current_temp))
-        #except:
-        #    logging.info("READ1")
-        #        
-        #    logging.info(self.temp)
-        #    current_temp = self.temp[-1]
-
+        # print(f"before making it a float {current_temp}")
         return float(current_temp)
 
     def update_plot_data(self):
-        # self.time = self.time[1:]  # Remove the first y element.
+        self.time = self.time[1:]  # Remove the first y element.
         self.time.append(self.time[-1] +1)  # Add a new value 1 higher than the last.
-        # self.temp = self.temp[1:]  # Remove the first
+        self.temp = self.temp[1:]  # Remove the first
 #         self.f_intensity = self.f_intensity[1:]
         try:
-            in_read = ser.readline().decode('utf-8').rstrip()
+            # in_read = ser.readline().decode('utf-8').rstrip()
+            in_read = recvFromArduino()
+            print(in_read)
+            self.temp.append(self.read_data(in_read))
+#             print(in_read)
             # A check to only accept full messages
-            if in_read[0] == "&" and in_read[-1] == "%":
-                self.temp.append(self.read_data(in_read)) # Add a new random value.
-            else:
-                print("read error 2")
-                logging.info("READ2")
-                logging.info(self.temp)
-                self.temp.append(self.temp[-1])
+            # if in_read[0] == "&" and in_read[-1] == "%":
+            #     self.temp.append(self.read_data(in_read)) # Add a new random value.
+            # else:
+            #     print("read error 2")
+            #     logging.info("READ2")
+            #     logging.info(self.temp)
+            #     self.temp.append(self.temp[-1])
         except Exception as e:
             print(in_read)
             print(e)
@@ -288,8 +279,51 @@ class Ui(QtWidgets.QMainWindow):
 
 #         self.ac_plot_ref.setData(self.cycle, self.f_intensity[:, -1])
 
+def recvFromArduino():
+    global startMarker, endMarker
+    
+    ck = ""
+    x = "z" # any value that is not an end- or startMarker
+    byteCount = -1 # to allow for the fact that the last increment will be one too many
+    
+    # wait for the start character
+    while  ord(x) != startMarker: 
+        x = ser.read()
+    
+    # save data until the end marker is found
+    while ord(x) != endMarker:
+        if ord(x) != startMarker:
+            ck = ck + x.decode("utf-8") # change for Python3
+            byteCount += 1
+        x = ser.read()
+    return(ck)            
+
+def waitForArduino():
+
+    # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
+    # it also ensures that any bytes left over from a previous message are discarded
+    
+    global startMarker, endMarker
+    
+    msg = ""
+    while msg.find("Arduino is ready") == -1:
+
+        while ser.inWaiting() == 0:
+            pass
+        
+        msg = recvFromArduino()
+        print("msg is 1")
+        print (msg) # python3 requires parenthesis
+        print()
+
 if __name__ == "__main__":
     # To save the log files, setting the name as the exact date time
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    startMarker = 60
+    endMarker = 62
+    # waitForArduino()
+    print("Arduino is ready2")
+    f_intensity = pd.DataFrame([0]*45)
     log_stamp = datetime.now()
     log_file_name = log_stamp.now()
     logging.basicConfig(filename = f"logs/{log_file_name}.log", filemode='w', format='%(levelname)s - %(message)s', level=logging.INFO)
